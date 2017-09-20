7ca909558adc9d817511edf465f87c013c30b682
collinl34 <collinlshelby@gmail.com>
2017-09-19 21:23:26 -0700

    component g along with styling and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    HomePageComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
